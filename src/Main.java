2f7c642a9df584ca0d3e83b657d357825312945e
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-09-06 21:07:52 +0200

    init commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Є тисячі доріг,");
+        System.out.print(" мільйон вузьких стежинок,\n");
+        System.out.print("Є тисячі ланів,");
+        System.out.println(" але один лиш мій.");
+        System.out.println("                     В.Симоненко");
+    }
+}
